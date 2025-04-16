8e449ac5114aa44020092fff9ed15b715a527b45
hyeok9855 <hyeok9855@gmail.com>
2025-04-16 18:40:06 +0900

    add slurm scripts

---
+++ gfn-diffusion/energy_sampling/scripts/0_run_sbatch.sh
@@ -0,0 +1,38 @@
+### Run default
+
+# TB = 2 x 2 = 4
+# DB = 2 x 2 x 2 = 8
+# SubTB = 2 x 2 x 2 x 2 = 16
+# Fldb = 2 x 2 x 2 = 8
+# FlSubTB = 2 x 2 x 2 x 2 = 16
+# SubTB chunking = 2 x 2 x 2 x 3 = 24
+# FlSubTB chunking = 2 x 2 x 2 x 3 = 24
+# Total # of experiments = 4 + 8 + 16 + 8 + 16 + 24 + 24 = 100
+
+for LOSS in tb db subtb fldb flsubtb subtb_chunking flsubtb_chunking; do
+    for ENERGY_NAME in gmm40 many_well; do
+        for BUFFER_PRIORITIZATION in normalized_iw none; do
+            if [ "$LOSS" = "tb" ]; then
+                sbatch scripts/default_${LOSS}.sh $ENERGY_NAME $BUFFER_PRIORITIZATION
+            elif [ "$LOSS" = "db" ] || [ "$LOSS" = "fldb" ]; then
+                for SHARE_EMBEDDINGS in embshare noembshare; do
+                    sbatch scripts/default_${LOSS}.sh $ENERGY_NAME $BUFFER_PRIORITIZATION $SHARE_EMBEDDINGS
+                done
+            elif [ "$LOSS" = "subtb" ] || [ "$LOSS" = "flsubtb" ]; then
+                for SHARE_EMBEDDINGS in embshare noembshare; do
+                    for LAMBDA in 1.5 2.0; do
+                        sbatch scripts/default_${LOSS}.sh $ENERGY_NAME $BUFFER_PRIORITIZATION $SHARE_EMBEDDINGS $LAMBDA
+                    done
+                done
+            elif [ "$LOSS" = "subtb_chunking" ] || [ "$LOSS" = "flsubtb_chunking" ]; then
+                for SHARE_EMBEDDINGS in embshare noembshare; do
+                    for CHUNK_RATIO in 0.05 0.1 0.2; do
+                        sbatch scripts/default_${LOSS}.sh $ENERGY_NAME $BUFFER_PRIORITIZATION $SHARE_EMBEDDINGS $CHUNK_RATIO
+                    done
+                done
+            else
+                echo "Invalid loss: $LOSS"
+            fi
+        done
+    done
+done
---
+++ gfn-diffusion/energy_sampling/scripts/default_flsubtb.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --partition=long
+#SBATCH --time=2-00:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:l40s:1
+#SBATCH -J energy_sampling
+#SBATCH -o .../gfn-is/slurm_logs/%x-%j.out
+
+cd .../gfn-is/gfn-diffusion/energy_sampling
+export PYTHONPATH=".:$PYTHONPATH"
+
+module --quiet purge
+module --quiet load anaconda/3
+module --quiet load cuda/12.6.0/cudnn/9.3
+conda activate gfn-is
+wandb login --relogin ...
+
+
+ENERGY_NAME=$1  # many_well, gmm40
+BUFFER_PRIORITIZATION=${2:-normalized_iw}  # normalized_iw, none
+SHARE_EMBEDDINGS=${3:-embshare}  # embshare, noembshare
+LAMBDA=${4:-2.0}
+
+if [ "$ENERGY_NAME" = "gmm40" ]; then
+    N_DIM=2
+    T_SCALE=100.0
+elif [ "$ENERGY_NAME" = "many_well" ]; then
+    N_DIM=32
+    T_SCALE=1.0
+fi
+
+EXP_NAME=flsubtb-l${LAMBDA}_buf-${BUFFER_PRIORITIZATION}_${SHARE_EMBEDDINGS}
+
+for SEED in 0 1 2 3 4; do
+    if [ "$SHARE_EMBEDDINGS" = "embshare" ]; then
+        python train.py \
+            --seed $SEED --energy_name $ENERGY_NAME --ndim $N_DIM --t_scale $T_SCALE --loss_type subtb --subtb_lambda $LAMBDA --eval_weighting --eval_buffer \
+            --partial_energy \
+            --prioritization $BUFFER_PRIORITIZATION --target_ess 0.05 --smoothing temper \
+            --exp_name $EXP_NAME &
+    else
+        python train.py \
+            --seed $SEED --energy_name $ENERGY_NAME --ndim $N_DIM --t_scale $T_SCALE --loss_type subtb --subtb_lambda $LAMBDA --eval_weighting --eval_buffer \
+            --partial_energy \
+            --prioritization $BUFFER_PRIORITIZATION --target_ess 0.05 --smoothing temper \
+            --no_share_embeddings \
+            --exp_name $EXP_NAME &
+    fi
+done
+wait
